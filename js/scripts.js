bda6521f82b4f0d6235170975401d99e679e7de9
D-Games <8223329+D-Games@users.noreply.github.com>
2024-01-11 18:15:27 +0000

    Comming the work for the new branch

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,39 @@ window.onbeforeunload = function(e) {
 };
 
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    var scrollPosition = window.scrollY || window.pageYOffset;
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible && scrollPosition > 0) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+function scrollMovement() {
+    var vHeader = document.querySelector(".vertical-header");
+    var scrollPosition = window.scrollY || window.pageYOffset
+    var totalPageHeight = document.body.scrollHeight - window.innerHeight;
+    var maxPosition = 20;
+
+    var newPosition = 0;
+
+    if (scrollPosition > 0) {
+        newPosition = (Math.min(scrollPosition / totalPageHeight, 1)) * maxPosition + 8;
+    }
+
+    vHeader.style.top = newPosition + "%";
+}
+
+window.addEventListener("scroll", scrollMovement);
