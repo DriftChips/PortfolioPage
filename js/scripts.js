8e03c82bf676a9912a7544d5b61d0ed291b6f6eb
D-Games <8223329+D-Games@users.noreply.github.com>
2024-01-19 16:32:51 +0000

    Commiting dynamic scalling changes

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ window.onbeforeunload = function(e) {
     localStorage.setItem('scrollpos', window.scrollY);
 };
 
+document.addEventListener("DOMContentLoaded", headerForMobile());
+
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
@@ -16,12 +18,15 @@ function reveal() {
         var windowHeight = window.innerHeight;
         var elementTop = reveals[i].getBoundingClientRect().top;
         var elementVisible = 150;
+        
+        if (screen.width > screen.height) {
+            if (elementTop < windowHeight - elementVisible && scrollPosition > 0) {
+                reveals[i].classList.add("active");
+            } else {
+                reveals[i].classList.remove("active");
+            }
+        } else return
 
-        if (elementTop < windowHeight - elementVisible && scrollPosition > 0) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
     }
 }
 
@@ -44,3 +49,25 @@ function scrollMovement() {
 }
 
 window.addEventListener("scroll", scrollMovement);
+
+function headerForMobile() {
+    var headerStyle = document.getElementById("home").style;
+    var projectContainers = document.querySelectorAll(".projects-inner-container");
+    var mobileSpacer = document.getElementById("mobile-spacer").style;
+
+    if (screen.width > screen.height) {
+        projectContainers.forEach(function(container) {
+            container.style.minHeight = "none";
+        });
+        headerStyle.position = "none";
+        headerStyle.flexDirection = "row";
+        mobileSpacer.height = "0px";
+    } else {
+        projectContainers.forEach(function(container) {
+            container.style.minHeight = "300px";
+        });
+        headerStyle.position = "fixed";
+        headerStyle.flexDirection = "column";
+        mobileSpacer.height = "300px";
+    }
+}
